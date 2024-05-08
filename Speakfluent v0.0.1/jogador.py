0e7e1b057a26e8b5a2294c3e0a6619062409d6fc
Gabz023 <103011583+Gabz023@users.noreply.github.com>
2024-05-08 15:39:58 -0300

    jogador.py

--- Speakfluent v0.0.1/jogador.py
+++ Speakfluent v0.0.1/jogador.py
@@ -4,7 +4,7 @@ from config import *
 class Jogador(pygame.sprite.Sprite):
     def __init__(self, pos, grupos, obst_sprites):
         super().__init__(grupos)
-        self.image = pygame.image.load('zelda/jogo/graphics/test/player.png').convert_alpha()
+        self.image = pygame.image.load('Speakfluent_v0.0.1/graphics/test/player.png').convert_alpha()
         self.rect = self.image.get_rect(topleft = pos)
         self.hitbox = self.rect.inflate(0, -26)
 
