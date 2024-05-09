9abb1851ebe5e54fabf1342e59e13346d110ccb4
Gabz023 <103011583+Gabz023@users.noreply.github.com>
2024-05-09 08:18:21 -0300

    tile.py

--- Speakfluent v0.0.1/tile.py
+++ Speakfluent v0.0.1/tile.py
@@ -4,6 +4,6 @@ from config import *
 class Tile(pygame.sprite.Sprite):
     def __init__(self, pos, grupos):
         super().__init__(grupos)
-        self.image = pygame.image.load('Speakfluent_v0.0.1/graphics/test/rock.png').convert_alpha()
+        self.image = pygame.image.load('graphics/test/rock.png').convert_alpha()
         self.rect = self.image.get_rect(topleft = pos)
         self.hitbox = self.rect.inflate(0,-10)
