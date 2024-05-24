3722f2d706e116dc68573964f22f42cdffd671c0
Gabz023 <103011583+Gabz023@users.noreply.github.com>
2024-05-24 17:41:21 -0300

    Delete Speakfluent v0.0.1 directory

--- Speakfluent v0.0.1/tile.py
+++
@@ -1,9 +0,0 @@
-import pygame
-from config import *
-
-class Tile(pygame.sprite.Sprite):
-    def __init__(self, pos, grupos):
-        super().__init__(grupos)
-        self.image = pygame.image.load('graphics/test/rock.png').convert_alpha()
-        self.rect = self.image.get_rect(topleft = pos)
-        self.hitbox = self.rect.inflate(0,-10)
